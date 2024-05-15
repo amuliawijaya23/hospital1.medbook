41ed94e93c1093916860f298003692f0b6a3e129
amuliawijaya23 <amuliawijaya@gmail.com>
2024-05-16 00:18:24 +0700

    sets up basic express server for client

---
+++ server/index.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const cors = require('cors');
+const http = require('http');
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+  });
+}
+
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+  console.log(`Server is now running on port ${PORT}`);
+});
