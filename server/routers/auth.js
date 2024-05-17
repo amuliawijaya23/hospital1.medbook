8fa046e6908156997983c062dbec9e2390f5475f
amuliawijaya23 <amuliawijaya@gmail.com>
2024-05-17 23:09:38 +0700

    adds middleware to logout call and removed unused routes

--- server/routers/auth.js
+++ server/routers/auth.js
@@ -1,15 +1,12 @@
 const { isAuthenticated } = require('../middleware');
 
 module.exports = (router) => {
-  router.get('/logout', async (req, res, next) => {
+  router.get('/logout', isAuthenticated, async (req, res, next) => {
     await req.logout((error) => {
       if (error) {
         return next(error);
       }
       return res.redirect('/');
     });
-  });
-  router.get('/user', isAuthenticated, (req, res) => {
-    res.json(req.user);
   });
 };
